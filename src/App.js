d7f973c4309311a56940772f29cca519dd4cb135
ahnjaemin <woalse1014@gmail.com>
2022-12-19 19:22:59 +0900

    코인API를 사용해서 화면에 출력하는 예제

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event) => setToDo(event.target.value);
-  const [toDos, setToDos] = useState([]);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((current) => [toDo, ...current]);
-    setToDo("");
-  }
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, [])
   return (
-  <div>
-    <h1>My ToDos({toDos.length})</h1>
-    <form onSubmit={onSubmit}>
-      <input type="text" onChange={onChange} value={toDo} placeholder="Write your to do..." />
-      <button>Add ToDo</button>
-    </form>
-    <hr />
-    <ul>
-      {toDos.map((item, index) => 
-        <li key={index}>{item}</li>
-      )}
-    </ul>
-  </div>
-  )
+    <div>
+      <h1>The Coins!({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((coin) => <li key={coin.id}>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD</li>)}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
