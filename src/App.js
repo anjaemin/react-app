7f23816b4b51942ae2c95aa8e68a079142f3fa75
ahnjaemin <woalse1014@gmail.com>
2022-12-20 19:53:20 +0900

    Movie, Home에 대한 route설정, Movie에 Link 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home"
 
 function App() {
-  return null;
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/movie" element={<Detail />}/>
+    </Routes>
+  </Router>;
 }
 
 export default App;
